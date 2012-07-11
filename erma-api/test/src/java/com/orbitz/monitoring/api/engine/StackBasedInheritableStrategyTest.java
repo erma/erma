c0940eedcc632f2a35b928a580e6d5c9f0141528
John VanderPol <John.VanderPol@Orbitz.com>
2012-07-11 09:44:33 -0500

    ERMA-32: Resolved issue with completing composite monitors that were never started.

--- erma-api/test/src/java/com/orbitz/monitoring/api/engine/StackBasedInheritableStrategyTest.java
+++ erma-api/test/src/java/com/orbitz/monitoring/api/engine/StackBasedInheritableStrategyTest.java
@@ -1,18 +1,30 @@
 package com.orbitz.monitoring.api.engine;
 
+import com.orbitz.monitoring.api.CompositeMonitor;
+import com.orbitz.monitoring.api.MonitoringLevel;
 import junit.framework.TestCase;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StackBasedInheritableStrategyTest extends TestCase {
 
+    private StackBasedInheritableStrategy strategy;
+
     protected void setUp() throws Exception {
-        super.setUp();
+        strategy = new StackBasedInheritableStrategy();
     }
 
-    public void test() {
+    public void testCompositeMonitorCompletedBelowMonitoringLevel() {
+        strategy.setEventPatternLevel(MonitoringLevel.INFO);
+        CompositeMonitor infoMonitor = mock(CompositeMonitor.class);
+        when(infoMonitor.getLevel()).thenReturn(MonitoringLevel.INFO);
+        strategy.compositeMonitorStarted(infoMonitor);
+        strategy.compositeMonitorCompleted(infoMonitor);
 
-    }
-
-    protected void tearDown() throws Exception {
-        super.tearDown();
+        CompositeMonitor debugMonitor = mock(CompositeMonitor.class);
+        when(debugMonitor.getLevel()).thenReturn(MonitoringLevel.DEBUG);
+        strategy.compositeMonitorStarted(debugMonitor);
+        strategy.compositeMonitorCompleted(debugMonitor);
     }
 }
