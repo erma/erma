3e02ab17d5bfd05f94bcc1d9b214146c9c486b76
Jacob Tomaw <jacob.tomaw@orbitz.com>
2013-08-20 15:18:28 -0500

    Added LoggingMonitoringEngineManager to simplify starting up with basic monitoring.

---
+++ erma-lib/src/java/com/orbitz/monitoring/lib/LoggingMonitoringEngineManager.java
@@ -0,0 +1,12 @@
+package com.orbitz.monitoring.lib;
+
+import com.orbitz.monitoring.lib.factory.ProcessGroup;
+import com.orbitz.monitoring.lib.factory.SimpleMonitorProcessorFactory;
+import com.orbitz.monitoring.lib.processor.LoggingMonitorProcessor;
+
+public class LoggingMonitoringEngineManager extends BaseMonitoringEngineManager {
+
+	public LoggingMonitoringEngineManager() {
+		super(new SimpleMonitorProcessorFactory(new ProcessGroup(new LoggingMonitorProcessor())));
+	}
+}
