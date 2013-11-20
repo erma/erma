c93cb25a6db05095b0d2e30725a17801e5b67405
Jacob Tomaw <JACOB.TOMAW@orbitz.com>
2013-11-20 08:53:23 -0600

    Continued clean up in preperation for extracting monitoring engine by focusing the tests for ThreadDefaultUncaughtExceptionHandler and made it more extensible

--- erma-lib/src/java/com/orbitz/monitoring/lib/interceptor/ThreadDefaultUncaughtExceptionHandler.java
+++ erma-lib/src/java/com/orbitz/monitoring/lib/interceptor/ThreadDefaultUncaughtExceptionHandler.java
@@ -1,19 +1,44 @@
 package com.orbitz.monitoring.lib.interceptor;
 
-import com.orbitz.monitoring.api.monitor.EventMonitor;
+import org.apache.commons.lang.exception.ExceptionUtils;
 
-import java.io.StringWriter;
-import java.io.PrintWriter;
+import com.orbitz.monitoring.api.Monitor;
+import com.orbitz.monitoring.api.monitor.EventMonitor;
 
 /**
  * This is an implementation of the {@link java.lang.Thread.UncaughtExceptionHandler} interface that
  * fires an EventMonitor whenever a Thread terminates due to an unhandled Throwable.
  *
- * The event that is fired will likely be routed to the SOC as an alarm.
- *
  * @author Matt O'Keefe
  */
 public class ThreadDefaultUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+    public static class EventFiringThreadExceptionHandler implements ThreadExceptionHandler {
+
+        @Override
+        public Monitor handleException(Thread thread, Throwable throwable) {
+            EventMonitor eventMonitor = new EventMonitor("ThreadTerminationDueToUncaughtThrowable");
+            eventMonitor.set("threadName", thread.getName());
+            eventMonitor.set("threadClass", thread.getClass().getName());
+            eventMonitor.set("stackTrace", ExceptionUtils.getStackTrace(throwable));
+            eventMonitor.fire();
+            return eventMonitor;
+        }
+    }
+
+    public static interface ThreadExceptionHandler {
+        Monitor handleException(Thread thread, Throwable throwable);
+    }
+
+    private ThreadExceptionHandler handler;
+    
+    public ThreadDefaultUncaughtExceptionHandler() {
+        this(new EventFiringThreadExceptionHandler());
+    }
+    
+    public ThreadDefaultUncaughtExceptionHandler(ThreadExceptionHandler handler) {
+        this.handler = handler;
+    }
 
     /**
      * Method invoked when the given thread terminates due to the
@@ -25,16 +50,10 @@ public class ThreadDefaultUncaughtExceptionHandler implements Thread.UncaughtExc
      * @param throwable the Throwable
      */
     public void uncaughtException(Thread thread, Throwable throwable) {
-        EventMonitor eventMonitor = new EventMonitor("ThreadTerminationDueToUncaughtThrowable");
-        eventMonitor.set("threadName", thread.getName());
-        eventMonitor.set("threadClass", thread.getClass().getName());
-        eventMonitor.set("stackTrace", getStackTrace(throwable));
-        eventMonitor.fire();
+        handler.handleException(thread, throwable);
     }
-
-    private String getStackTrace(Throwable t) {
-        StringWriter out = new StringWriter();
-        t.printStackTrace(new PrintWriter(out));
-        return out.toString();
+    
+    public ThreadExceptionHandler getHandler() {
+        return handler;
     }
 }
--- erma-lib/test/src/java/com/orbitz/monitoring/lib/interceptor/ThreadDefaultUncaughtExceptionHandlerTest.java
+++ erma-lib/test/src/java/com/orbitz/monitoring/lib/interceptor/ThreadDefaultUncaughtExceptionHandlerTest.java
@@ -1,69 +1,44 @@
 package com.orbitz.monitoring.lib.interceptor;
 
 import junit.framework.TestCase;
-import com.orbitz.monitoring.lib.processor.CPUProfilingMonitorProcessor;
-import com.orbitz.monitoring.test.MockMonitorProcessor;
-import com.orbitz.monitoring.test.MockMonitorProcessorFactory;
-import com.orbitz.monitoring.test.MockDecomposer;
-import com.orbitz.monitoring.api.MonitorProcessor;
-import com.orbitz.monitoring.api.MonitoringEngine;
+
 import com.orbitz.monitoring.api.Monitor;
-import com.orbitz.monitoring.api.monitor.TransactionMonitor;
+import com.orbitz.monitoring.api.monitor.AbstractMonitor;
+import com.orbitz.monitoring.lib.interceptor.ThreadDefaultUncaughtExceptionHandler.EventFiringThreadExceptionHandler;
+import com.orbitz.monitoring.lib.interceptor.ThreadDefaultUncaughtExceptionHandler.ThreadExceptionHandler;
 
 /**
- * Unit tests for the ThreadDefaultUncaughtExceptionHandlerTe.
+ * Unit tests for the ThreadDefaultUncaughtExceptionHandler.
  *
  * <p>(c) 2000-06 Orbitz, LLC. All Rights Reserved.
  *
  * @author Matt O'Keefe
  */
 public class ThreadDefaultUncaughtExceptionHandlerTest extends TestCase {
-    // ** PRIVATE DATA ********************************************************
-
-    private MockMonitorProcessor _mockMonitorProcessor = new MockMonitorProcessor();
-
-    // ** TEST SUITE METHODS **************************************************
-    protected void setUp()
-            throws Exception {
-
-        super.setUp();
-
-        MonitorProcessor[] processors = new MonitorProcessor[1];
-        processors[0] = _mockMonitorProcessor;
-        MonitoringEngine.getInstance().setProcessorFactory(
-                new MockMonitorProcessorFactory(processors));
-        MonitoringEngine.getInstance().setDecomposer(new MockDecomposer());
-        MonitoringEngine.getInstance().startup();
-
-    }
-
-    protected void tearDown()
-            throws Exception {
-        super.tearDown();
-
-        MonitoringEngine.getInstance().shutdown();
-    }
-
-    // ** TEST METHODS ********************************************************
     public void testThreadTerminationDueToUncaughtThrowable() {
+        final Thread thisThread = Thread.currentThread();
+        final Throwable expected = new RuntimeException();
+        final Monitor monitor = new AbstractMonitor() {};
+        ThreadDefaultUncaughtExceptionHandler handler = new ThreadDefaultUncaughtExceptionHandler(new ThreadExceptionHandler(){
 
-        _mockMonitorProcessor.clear();
-        Thread.setDefaultUncaughtExceptionHandler(new ThreadDefaultUncaughtExceptionHandler());
-        new Thread(new Runnable() {
-            public void run() {
-                throw new RuntimeException();
-            }
-        }).start();
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        
-        Monitor[] monitors = _mockMonitorProcessor.extractProcessObjects();
-        Monitor monitor = monitors[0];
+            @Override
+            public Monitor handleException(Thread thread, Throwable throwable) {
+                assertEquals(thisThread, thread);
+                assertEquals(expected, throwable);
+                monitor.set("handled", true);
+                return monitor;
+            }});
+        handler.uncaughtException(thisThread, expected);
+        assertTrue(monitor.hasAttribute("handled"));
+    }
+    
+    public void testDefaultHandler() {
+        final Thread thread = Thread.currentThread();
+        final Throwable throwable = new RuntimeException();
+        ThreadExceptionHandler handler = new ThreadDefaultUncaughtExceptionHandler().getHandler();
+        Monitor monitor = handler.handleException(thread, throwable);
         assertEquals("Didn't find a ThreadTerminationDueToUncaughtThrowable Monitor",
-                monitor.get(Monitor.NAME), "ThreadTerminationDueToUncaughtThrowable");
+        monitor.get(Monitor.NAME), "ThreadTerminationDueToUncaughtThrowable");
         assertTrue("Didn't find a threadName attribute", monitor.hasAttribute("threadName"));
         assertTrue("Didn't find a threadClass attribute", monitor.hasAttribute("threadClass"));
         assertTrue("Didn't find a stackTrace attribute", monitor.hasAttribute("stackTrace"));
